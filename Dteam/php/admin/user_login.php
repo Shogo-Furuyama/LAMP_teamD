122c97183f5f2725da65c24e1645a1e32e47f0a7
Shi-ori <theworld1109@icloud.com>
2023-06-28 15:01:06 +0900

    ログインのdb、新規登録のdb系の作業あとログインの正規表現

---
+++ Dteam/php/admin/user_login.php
@@ -0,0 +1,68 @@
+<?php
+/*!
+@file login.php
+@brief  ログイン（フロント）
+@copyright Copyright (c) 2017 Yamanoi Yasushi.
+*/
+
+require_once("inc_base.php");
+require_once($CMS_COMMON_INCLUDE_DIR . "libs.php");
+require_once("inc_smarty.php");
+
+$ERR_STR = "";
+$user_id = "";
+$user_name = "";
+
+session_start();
+if (isset($_SESSION['tmD2023_mem']['err']) && $_SESSION['tmD2023_mem']['err'] != "") {
+    $ERR_STR = $_SESSION['tmD2023_mem']['err'];
+}
+
+//このセッションをクリア
+$_SESSION['tmD2023_mem'] = array();
+
+if (!empty($_POST['mail']) && !empty($_POST['password'])) 
+{ 
+    $cuser = new cuser;
+    //親クラスのselect()メンバ関数を呼ぶ
+    $result = $cuser->get_tgt_login(false,$_POST['mail'],$_POST['password']);
+    if(!empty($result)){
+        session_start();
+    $_SESSION['tmD2023_mem']['user_id'] = $result['user_id'];
+    $_SESSION['tmD2023_mem']['user_name'] = $result['user_name'];
+    cutil::redirect_exit("index.php");
+    }else{
+        global $ERR_STR;
+        $ERR_STR ="メールアドレスまたはパスワードが一致しません。\n";
+        
+    }
+}
+
+
+function chk_member_login($member_login, $member_password)
+{
+    global $ERR_STR;
+    global $user_id;
+    global $user_name;
+    $member = new cmember();
+    $row = $member->get_tgt_login(false, $member_login);
+    if ($row === false || !isset($row['user_id'])) {
+        $ERR_STR .= "ログイン名が不定です。\n";
+        return false;
+    }
+    //暗号化によるパスワード認証
+    if (!cutil::pw_check($member_password, $row['enc_password'])) {
+        $ERR_STR .= "パスワードが違っています。\n";
+        return false;
+    }
+    $user_id = $row['user_id'];
+    $user_name = $row['user_name'];
+    return true;
+}
+
+$smarty->assign('ERR_STR', $ERR_STR);
+//Smartyを使用した表示(テンプレートファイルの指定)
+$smarty->display('user_login.tmpl');
+
+
+?>
--- Dteam/php/common/contents_db.php
+++ Dteam/php/common/contents_db.php
@@ -574,23 +574,24 @@ user_table.credit_id = credit_table.credit_id" //条件
     @return 配列（1次元配列になる）空の場合はfalse
     */
     //--------------------------------------------------------------------------------------
-    public function get_tgt_login($debug, $login)
+    public function get_tgt_login($debug, $mail,$pass)
     {
-        if ($login == '') {
+        if ($mail == '' && $pass == '') {
             //falseを返す
             return false;
         }
-        $safe_login = $this->make_safe_sqlstr((string) $login);
+        $safe_mail = $this->make_safe_sqlstr((string) $mail);
+        $safe_password = $this->make_safe_sqlstr((string)$pass);
         //親クラスのselect()メンバ関数を呼ぶ
         $this->select(
             $debug,
             //デバッグ表示するかどうか
-            "user_table.*,credit_table.credit_number",
+            "user_id,user_name",
             //取得するカラム
-            "user_table,credit_table",
+            "user_table",
             //取得するテーブル
-            "user_table.user_table_login like {$safe_login} and 
-user_table.credit_id = credit_table.credit_id" //条件
+            "mail = {$safe_mail} AND password = {$safe_password}"
+             //条件
         );
         return $this->fetch_assoc();
     }
@@ -964,136 +965,6 @@ class clist extends crecord
         parent::__destruct();
     }
 }
-
-
-
-
-
-
-
-
-
-///	ログインクラス
-//--------------------------------------------------------------------------------------
-class clogin extends crecord
-{
-    //--------------------------------------------------------------------------------------
-    /*!
-       @brief	コンストラクタ
-       */
-    //--------------------------------------------------------------------------------------
-    public function __construct()
-    {
-        //親クラスのコンストラクタを呼ぶ
-        parent::__construct();
-    }
-    //--------------------------------------------------------------------------------------
-    /*!
-       @brief	すべての個数を得る
-       @param[in]	$debug	デバッグ出力をするかどうか
-       @return	個数
-       */
-    //--------------------------------------------------------------------------------------
-    public function get_all_count($debug)
-    {
-        //親クラスのselect()メンバ関数を呼ぶ
-        $this->select(
-            $debug,
-            //デバッグ文字を出力するかどうか
-            "count(*)",
-            //取得するカラム
-            "login_table",
-            //取得するテーブル
-            "1" //条件
-        );
-        if ($row = $this->fetch_assoc()) {
-            //取得した個数を返す
-            return $row['count(*)'];
-        } else {
-            return 0;
-        }
-    }
-    //--------------------------------------------------------------------------------------
-    /*!
-       @brief	指定された範囲の配列を得る
-       @param[in]	$debug	デバッグ出力をするかどうか
-       @param[in]	$from	抽出開始行
-       @param[in]	$limit	抽出数
-       @return	配列（2次元配列になる）
-       */
-    //--------------------------------------------------------------------------------------
-    public function get_all($debug, $from, $limit)
-    {
-        $arr = array();
-        //親クラスのselect()メンバ関数を呼ぶ
-        $this->select(
-            $debug,
-            //デバッグ表示するかどうか
-            "*",
-            //取得するカラム
-            "login_table",
-            //取得するテーブル
-            "1",
-            //条件
-            "login_id asc", //並び替え
-            "limit " . $from . "," . $limit //抽出開始行と抽出数
-        );
-        //順次取り出す
-        while ($row = $this->fetch_assoc()) {
-            $arr[] = $row;
-        }
-        //取得した配列を返す
-        return $arr;
-    }
-    //--------------------------------------------------------------------------------------
-    /*!
-       @brief	指定されたIDの配列を得る
-       @param[in]	$debug	デバッグ出力をするかどうか
-       @param[in]	$id		ID
-       @return	配列（1次元配列になる）空の場合はfalse
-       */
-    //--------------------------------------------------------------------------------------
-    public function get_tgt($debug, $id)
-    {
-        if (
-            !cutil::is_number($id)
-            || $id < 1
-        ) {
-            //falseを返す
-            return false;
-        }
-        //親クラスのselect()メンバ関数を呼ぶ
-        $this->select(
-            $debug,
-            //デバッグ表示するかどうか
-            "*",
-            //取得するカラム
-            "login_table", //取得するテーブル
-            "login_id=" . $id //条件
-        );
-        return $this->fetch_assoc();
-    }
-    //--------------------------------------------------------------------------------------
-    /*!
-       @brief	デストラクタ
-       */
-    //--------------------------------------------------------------------------------------
-    public function __destruct()
-    {
-        //親クラスのデストラクタを呼ぶ
-        parent::__destruct();
-    }
-}
-
-
-
-
-
-
-
-
-
-
 
 //以下、DBクラス使用例
 
